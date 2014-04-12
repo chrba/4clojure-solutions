4855ea9736a27b354e5c105d74143916f7f3d341
Christian Bannes <cbannes@gmail.com>
2014-04-12 14:38:03 +0200

    add description

--- solutions.clj
+++ solutions.clj
@@ -53,7 +53,11 @@
 
 
 
-
+;112. Sequs Horribilis
+;Create a function which takes an integer and a nested collection of integers 
+;as arguments. Analyze the elements of the input collection and return a sequence 
+;which maintains the nested structure, and which includes all elements starting
+; from the head whose sum is less than or equal to the input integer.
 (fn sequs-horribilis
   [n coll]
   (letfn [(traverse [c sum max]
